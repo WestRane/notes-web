7dc2681d01248ae86b899425f501fa93529fda5a
WestRane <268738597+WestRane@users.noreply.github.com>
2026-04-20 12:33:03 +0300

    fix(search): trigger hotkey regardless of a keyboard layout

--- quartz/components/scripts/search.inline.ts
+++ quartz/components/scripts/search.inline.ts
@@ -251,12 +251,14 @@ async function setupSearch(searchElement: Element, currentSlug: FullSlug, data: 
 
   let currentHover: HTMLInputElement | null = null
   async function shortcutHandler(e: HTMLElementEventMap["keydown"]) {
-    if (e.key === "s" && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
+    const isSPressed = e.code === "KeyS";
+    
+    if (isSPressed && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
       e.preventDefault()
       const searchBarOpen = container.classList.contains("active")
       searchBarOpen ? hideSearch() : showSearch("basic")
       return
-    } else if (e.shiftKey && (e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "s") {
+    } else if (e.shiftKey && (e.ctrlKey || e.metaKey) && isSPressed) {
       // Hotkey to open tag search
       e.preventDefault()
       const searchBarOpen = container.classList.contains("active")
